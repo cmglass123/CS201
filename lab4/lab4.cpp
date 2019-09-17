5ebbf8061597f86dacded7f277ca9de5878b42f9
Christopher Glass <cmglass@alaska.edu>
2019-09-17 11:06:23 -0800

    including the things for making a file and reading it

---
+++ lab4/lab4.cpp
@@ -0,0 +1,15 @@
+// lab4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+
+}
+
