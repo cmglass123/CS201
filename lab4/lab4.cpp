1c4c32545c82ecafaa72ea3b2a2d6a6d448f2f54
Christopher Glass <cmglass@alaska.edu>
2019-09-17 11:17:05 -0800

    added a way to add things to lab4.txt

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -10,9 +10,22 @@ using std::endl;
 
 int main()
 {
+	int times;
+	std::string word;
+
 	std::ofstream ofile("lab4.txt");
 
 	std::ifstream ifile("lab4.txt");
+	
+	cout << "enter a number and a word: ";
+	cin >> times;
+	cin >> word;
+
+	for (int x = 0; x >= times; x++)
+	{
+		ifile >> word;
+	}
+	ofile << word;
 
 }
 
