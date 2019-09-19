d1e0205138eb323c6c66c881a314c468ac07dc2c
Christopher Glass <cmglass@alaska.edu>
2019-09-18 21:49:49 -0800

    made the member function from box.hpp in the box.cpp file and also added a int to caulate area.

--- HW01/HW01/box.cpp
+++ HW01/HW01/box.cpp
@@ -5,8 +5,27 @@ box.cpp file for homework #1
 */
 
 #include <iostream>
+#include "box.hpp"
+using std::cout;
 
-int main()
+Box::Box()
 {
+	cout << "the box is being made\n";
+}
 
+void Box::theLength(int l)// the int is a lowcase L it just looks like the number 1
+{
+	length = l;
+}
+
+void Box::theWidth(int w)
+{
+	width = w;
+}
+
+int Box::area()
+{
+	int a;
+	a = length * width;
+	return a;
 }
