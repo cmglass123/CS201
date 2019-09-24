4819c6e86ed5df3ef0add5623ff8a014ea524813
Christopher Glass <cmglass@alaska.edu>
2019-09-24 09:58:43 -0800

    putting the main include and using std::  files that we will be using today

---
+++ Lab5/Lab5.cpp
@@ -0,0 +1,18 @@
+/*
+Christopher Glass
+9/24/2019
+Lab #5 {Binary Files}
+write a program thats able to open,read,and print a data file
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <fstream>
+using std::ifstream;
+
+int main()
+{
+
+}
