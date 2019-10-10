ce5d2d8666fc2c97417ace978c04b699a196c1b4
Christopher Glass <cmglass@alaska.edu>
2019-10-10 11:08:04 -0800

    fixed the promblem

--- lab8[overload arithmetic]/lab8[overload arithmetic].cpp
+++ lab8[overload arithmetic]/lab8[overload arithmetic].cpp
@@ -1,17 +1,20 @@
 /*
-
+christopher Glass
+10/03/2019
+Lab #8 [overloaded arithmetic operator]
 */
 
 #include <iostream>
 #include "complex.h"
 using std::cout;
+using std::endl;
 
 int main()
 {
-	Complex a[2];
-	Complex b[65];
+	Complex a{ 2 };
+	Complex b{ 13 };
 
-	cout << a + b;
-
-	cout << 1 + Complex(2,3) << std::endl;
+	cout << a + b << endl;
+	
+	cout << 1 + Complex(2,3) << endl;
 }
