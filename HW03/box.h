93f7aca462cfe5dc4adcc00ef4709de2a1577f54
Christopher Glass <cmglass@alaska.edu>
2019-10-15 11:03:44 -0800

    made a box class with a bool _isfilled that is set to true and a Box member that takes three parameter

---
+++ HW03/box.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef BOX_HPP_
+#define BOX_HPP_
+
+#include <string>
+#include <sstream>
+
+class Box
+{
+public:
+	Box();
+	Box(int w, int h);
+	Box(int w, int h, bool filled);
+
+	int getHight() const;
+	int getWidth() const;
+
+	void setHight(int h);
+	void setWidth(int w);
+
+	void print(std::ostringstream& os) const;
+	void print();
+
+private:
+	int _hight;
+	int _width;
+	bool _isFilled = true;
+};
+
+
+#endif // !BOX_HPP_
--- HW04/HW04.cpp
+++ HW04/HW04.cpp
@@ -1,4 +1,4 @@
-// HW2 New.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// HW04.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
