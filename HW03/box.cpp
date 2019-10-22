dc1b9dcc9391f955e8d8a0cc7164df23e7068071
Christopher Glass <cmglass@alaska.edu>
2019-10-22 10:01:47 -0800

    made the print members for the box2 file

--- HW03/box.cpp
+++ HW03/box.cpp
@@ -1,4 +1,7 @@
 #include "box.h"
+#include<iomanip>
+#include <iostream>
+using std::cout;
 
 Box::Box() {}
 
@@ -24,4 +27,70 @@ int Box::getHight() const
 int Box::getWidth() const
 {
 	return _width;
+}
+
+void Box::print(std::ostringstream& os) const
+{
+	if (_isFilled)
+	{
+		for (int rows = 1; rows <= _hight; rows++)
+		{
+			for (int column = 1; column <= _width; column++)
+			{
+				os << "x";
+			}
+			os << std::endl;
+		}
+	}
+	else
+	{
+		for (int rows = 1; rows <= _hight; rows++)
+		{
+			for (int column = 1; column <= _width; column++)
+			{
+				if (rows == 1 | column == 1 | column == _width | rows == _hight)
+				{
+					os << "x";
+				}
+				else
+				{
+					os << " ";
+				}
+			}
+			os << std::endl;
+		}
+	}
+}
+
+void Box::print()
+{
+	if (_isFilled)
+	{
+		for (int rows = 1; rows <= _hight; rows++)
+		{
+			for (int column = 1; column <= _width; column++)
+			{
+				std::cout << "x";
+			}
+			std::cout <<std::endl;
+		}
+	}
+	else
+	{
+		for (int rows = 1; rows <= _hight; rows++)
+		{
+			for (int column = 1; column <= _width; column++)
+			{
+				if (rows == 1 | column == 1 | column == _width | rows == _hight)
+				{
+					std::cout << "x";
+				}
+				else
+				{
+					std::cout << " ";
+				}
+			}
+			std::cout << std::endl;
+		}
+	}
 }
